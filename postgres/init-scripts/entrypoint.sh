5ba1b72ecc853170652e02ce4c627d6b37c8ee19
dagahan <nikitosu27@gmail.com>
2025-07-15 04:39:53 +0700

    before integration of template

--- postgres/init-scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-envsubst < /init-scripts/init-template.sql > /init-scripts/init.sql
-
-cp /init-scripts/init.sql /docker-entrypoint-initdb.d/init.sql
-
-exec docker-entrypoint.sh postgres "$@"
